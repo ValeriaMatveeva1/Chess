b3c22fa946c77a0d2797fc5a72897a825df1304e
Spyke09 <romanov.alexanderromanov@yandex.ru>
2022-02-13 13:14:23 +0300

    some changes

--- scr/vector.h
+++ scr/vector.h
@@ -6,10 +6,9 @@
 
 #define _vector_t(T) struct{T *data; int len; int maxlen;}
 #define v_init(v) memset((v), 0, sizeof(*(v)))
-#define v_clear(v) ( free((v)->data), vec_init(v) ) 
+#define v_clear(v) ( free((v)->data), v_init(v) ) 
 
 #define _vector_args(v) (char**)(&(v)->data), &(v)->len, &(v)->maxlen, sizeof(*(v)->data)
-
 
 #define v_push(v, val) (_vector_expand(_vector_args(v)) ? -1 :((v)->data[(v)->len++] = (val), 0), 0)
 
@@ -17,7 +16,9 @@
 
 #define v_pop(v, idx) (_vector_pop(_vector_args(v), idx) ? -1 :(v)->len--, 0)
 
-int v_expand(char** data, int *len, int *maxlen, int size);
+#define v_find(v, val, idx) for ((idx) = 0; (idx) < (v)->len; (idx)++) {if ((v)->data[(idx)] == (val)) break;} if ((idx) == (v)->len) (idx) = -1;
+
+int _vector_expand(char** data, int *len, int *maxlen, int size);
 int _vector_insert(char** data, int *len, int *maxlen, int size, int idx);
 int _vector_pop(char** data, int *len, int *maxlen, int size, int idx);
 
