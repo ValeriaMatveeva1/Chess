b67bb7aba941a2c6649e36e32934f87579831d9a
ValeriaMatveeva1 <matveevavalerochka11@gmail.com>
2022-02-22 17:27:15 +0300

    Added full names of pieces and possible moves

--- src/main.c
+++ src/main.c
@@ -1,17 +1,15 @@
-#include <stdio.h>
-#include <windows.h>
-#define win_len 1000
-#define win_height 700
-#define box_len 80
-
+#include "main.h"
 
 HWND bt_color;
 HWND bt_again;
-HWND label;
+HWND label, label2;
 
 HWND labels[8][8];
 
 char color = 'w';
+
+vector_int_t possible_turns;
+
 
 const char new_board[8][8][2] = {
         {{'r','w'},{'n','w'},{'b','w'},{'q','w'},{'k','w'},{'b','w'},{'n','w'},{'r','w'}},
@@ -27,6 +25,7 @@ const char new_board[8][8][2] = {
 char board[8][8][2];
 int first_click = 0;
 int player_turn;
+int enemy_turn = -1;
 
 void game_restart()
 {
@@ -46,16 +45,36 @@ int from_win_coord(int x, int y)
     return -1;
 }
 
+char* long_fig_name(char short_name[2])
+{
+    if (short_name[1]=='w'){
+        if (short_name[0]=='p') return "\nwhite\npawn\0";
+        if (short_name[0]=='n') return "\nwhite\nknight\0";
+        if (short_name[0]=='b') return "\nwhite\nbishop\0";
+        if (short_name[0]=='q') return "\nwhite\nqueen\0";
+        if (short_name[0]=='k') return "\nwhite\nking\0";
+        if (short_name[0]=='r') return "\nwhite\nrock\0";
+    } else if (short_name[1]=='b') {
+        if (short_name[0]=='p') return "\nblack\npawn\0";
+        if (short_name[0]=='n') return "\nblack\nknight\0";
+        if (short_name[0]=='b') return "\nblack\nbishop\0";
+        if (short_name[0]=='q') return "\nblack\nqueen\0";
+        if (short_name[0]=='k') return "\nblack\nking\0";
+        if (short_name[0]=='r') return "\nblack\nrock\0";
+    } else {
+        return "\0";
+    }
+
+}
+
 void re_draw_board()
 {
     for (int i = 0; i<8; ++i){
         for (int j = 0; j<8; ++j){
             if (color=='w'){
-                char temp[3]; temp[0] = board[7-i][j][0]; temp[1] = board[7-i][j][1]; temp[2] = '\0';
-                SetWindowText(labels[i][j], temp[0]!='0'?temp:'\0');
+                SetWindowText(labels[i][j], long_fig_name(board[7-i][j]));
             } else {
-                char temp[3]; temp[0] = board[i][7-j][0]; temp[1] = board[i][7-j][1]; temp[2] = '\0';
-                SetWindowText(labels[i][j], temp[0]!='0'?temp:'\0');
+                SetWindowText(labels[i][j], long_fig_name(board[i][7-j]));
             }
 
         }
@@ -65,20 +84,26 @@ void re_draw_board()
 void draw_turn(int player_turn)
 {
     int a = player_turn/100, b = player_turn%100;
-    if (player_turn==-1) return;
+    if (player_turn==-100-1) return;
     char *rt1 = to_str(a); char* rt2 = to_str(b);
-    printf("%c%c %c%c\n", rt1[0], rt1[1], rt2[0], rt2[1]);
+    /*printf("%c%c %c%c\n", rt1[0], rt1[1], rt2[0], rt2[1]);*/
     if (add_turn(board, player_turn/100, player_turn%100, color)) {
-        int bt = minimax(board, color=='w'?'b':'w', 2);
+        SetWindowText(label2, "Bot turn.");
+        re_draw_board();
+        int bt = minimax(board, color=='w'?'b':'w', 4);
+        enemy_turn = bt;
         write_turn(board, bt/100, bt%100);
+        int kp = king_position(board, color);
+        if (check_checkmate(board, kp/10, kp%10, color)){
+            SetWindowText(label2, "Check.");
+        } else {
+            SetWindowText(label2, "Your turn.");
+        }
     }
-    if (loss_q(board, color)) SetWindowText(label, "You lose");
-    if (loss_q(board, color=='w'?'b':'w')) SetWindowText(label, "You win");
+    if (loss_q(board, color)) SetWindowText(label, "You lose.");
+    if (loss_q(board, color=='w'?'b':'w')) SetWindowText(label, "You win.");
     re_draw_board();
 }
-
-
-
 
 LRESULT WINAPI DefWindProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam)
 {
@@ -90,6 +115,7 @@ LRESULT WINAPI DefWindProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam
         if (first_click==0){
             player_turn = from_win_coord(x, y);
             first_click = 1;
+            possible_turns = turns(board, player_turn);
             re_draw_board();
         } else {
             first_click = 0;
@@ -98,41 +124,72 @@ LRESULT WINAPI DefWindProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam
         }
     }
     else if (message == WM_COMMAND){
-        printf("WM_COMMAND\n");
         if (bt_color == lparam){
+            enemy_turn = -1;
+            v_clear(&possible_turns);
             color = (color=='w'? 'b':'w');
             SetWindowText(label, color=='w'? "You are playing white.":"You are playing black.");
             game_restart();
             if (color=='b'){
                 int bt = minimax(board, 'w', 2);
+                enemy_turn = bt;
                 write_turn(board, bt/100, bt%100);
                 re_draw_board();
             }
         }
         if (bt_again == lparam) {
+            v_clear(&possible_turns);
+            enemy_turn = -1;
             SetWindowText(label, color=='w'? "You are playing white.":"You are playing black.");
             game_restart();
             if (color=='b'){
                 int bt = minimax(board, 'w', 2);
+                enemy_turn = bt;
                 write_turn(board, bt/100, bt%100);
                 re_draw_board();
             }
         }
     } else if (message==WM_CTLCOLORSTATIC) {
-        if (color=='w' && first_click && lparam==labels[7-player_turn%10][player_turn/10]){
-            SetBkMode((HDC)wparam, TRANSPARENT);
-            return CreateSolidBrush(RGB(201,201,201));
-        } else if (color=='b' && first_click && lparam==labels[player_turn%10][7-player_turn/10]){
-            SetBkMode((HDC)wparam, TRANSPARENT);
-            return CreateSolidBrush(RGB(201,201,201));
-        } else {
-            int temp = 0;
-            for (int i = 0; i<8; ++i)
-                for (int j = 0; j<8; ++j) if ((i%2+j%2)%2 && lparam==labels[i][j]) {temp = 1; break;};
-            if (temp){
+        if (color=='w'){
+            if (first_click && lparam==labels[7-player_turn%10][player_turn/10]){
                 SetBkMode((HDC)wparam, TRANSPARENT);
-                return CreateSolidBrush(RGB(220,220,220));
+                return CreateSolidBrush(RGB(201,201,201));
             }
+            int temp = 0;
+            for (int i = 0; i<possible_turns.len; ++i)
+            if (labels[7-possible_turns.data[i]%10][possible_turns.data[i]/10] == lparam) {temp = 1; break;}
+            if (first_click && temp && player_turn!=-1 && board[player_turn%10][player_turn/10][1]==color){
+                SetBkMode((HDC)wparam, TRANSPARENT);
+                return CreateSolidBrush(RGB(193,255,184));
+            }
+            if (enemy_turn!=-1 && (lparam==labels[7-(enemy_turn/100)%10][(enemy_turn/100)/10] || lparam==labels[7-(enemy_turn%100)%10][(enemy_turn%100)/10])){
+                SetBkMode((HDC)wparam, TRANSPARENT);
+                return CreateSolidBrush(RGB(255,185,185));
+            }
+
+        } else if (color=='b'){
+            if (first_click && lparam==labels[player_turn%10][7-player_turn/10]){
+                SetBkMode((HDC)wparam, TRANSPARENT);
+                return CreateSolidBrush(RGB(201,201,201));
+            }
+            int temp = 0;
+            for (int i = 0; i<possible_turns.len; ++i)
+            if (labels[possible_turns.data[i]%10][7-possible_turns.data[i]/10] == lparam) {temp = 1; break;}
+            if (first_click && temp && player_turn!=-1 && board[player_turn%10][player_turn/10][1]==color){
+                SetBkMode((HDC)wparam, TRANSPARENT);
+                return CreateSolidBrush(RGB(193,255,184));
+            }
+            if (enemy_turn!=-1 && (lparam==labels[(enemy_turn/100)%10][7-(enemy_turn/100)/10] || lparam==labels[(enemy_turn%100)%10][7-(enemy_turn%100)/10])){
+                SetBkMode((HDC)wparam, TRANSPARENT);
+                return CreateSolidBrush(RGB(255,185,185));
+            }
+        }
+        int temp = 0;
+        for (int i = 0; i<8; ++i)
+            for (int j = 0; j<8; ++j) if ((i%2+j%2)%2 && lparam==labels[i][j]) {temp = 1; break;};
+        if (temp){
+            SetBkMode((HDC)wparam, TRANSPARENT);
+            return CreateSolidBrush(RGB(220,220,220));
         }
     }
     else return DefWindowProcA(hwnd, message, wparam, lparam);
@@ -157,13 +214,14 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 
     label = CreateWindow("static", "You are playing white.", WS_VISIBLE | WS_CHILD, win_len-200, win_height/2+85, 150, 50, hwnd, NULL, NULL, NULL);
+    label2 = CreateWindow("static", "Your turn.", WS_VISIBLE | WS_CHILD, win_len-200, win_height/2+140, 150, 50, hwnd, NULL, NULL, NULL);
 
     game_restart();
+    v_init(&possible_turns);
 
     for (int i = 0; i<8; ++i){
         for (int j = 0; j<8; ++j){
-            char temp[3]; temp[0] = board[7-i][j][0]; temp[1] = board[7-i][j][1]; temp[2] = '\0';
-            labels[i][j] = CreateWindow("static", temp[0]!='0'?temp:'\0', WS_VISIBLE | WS_CHILD| SS_CENTER| WS_EX_CLIENTEDGE, 10+(box_len+1)*j, 10+(box_len+1)*i, box_len, box_len, hwnd, NULL, NULL, NULL);
+            labels[i][j] = CreateWindow("static", long_fig_name(board[7-i][j]), WS_VISIBLE | WS_CHILD| SS_CENTER, 10+(box_len+1)*j, 10+(box_len+1)*i, box_len, box_len, hwnd, NULL, NULL, NULL);
         }
     }
 
--- src/main.h
+++ src/main.h
@@ -1,3 +1,16 @@
 #include "chess_engine.h"
 #include "bot_logic.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include <windows.h>
+#define win_len 1000
+#define win_height 700
+#define box_len 80
+
+
+void game_restart();
+int from_win_coord(int x, int y);
+char* long_fig_name(char short_name[2]);
+void re_draw_board();
+void draw_turn(int player_turn);
+LRESULT WINAPI DefWindProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam);
